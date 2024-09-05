0cf802e55d277e7fd74c3e901ca8edda43a2fc1c
nathalialimaa <152560098+nathalialimaa@users.noreply.github.com>
2024-09-05 23:37:01 +0000

    criando tabelas no banco de dados

--- draft.py
+++ draft.py
@@ -1,54 +1,34 @@
-from functions.create_db import create_db
-
-create_db('meuqueridobanco')
-
-import sqlite3
-
-def create_db(
-    database_name: str
-) -> sqlite3.Connection:
-    """
-    Create a new SQLite database or connect to an existing one.
-
-    Args:
-    database_name (str): The name of the database to create or connect to.
-
-    Returns:
-    sqlite3.Connection: A connection object to the SQLite database.
-    """
-    return sqlite3.connect(f'{database_name}.db')
-
-
-
-
-
-
-
-
 import pandas as pd
 import sqlite3
-data= pd.DataFrame({
-    'ID':[1,2,3,4],
+
+# create a data example
+data = pd.DataFrame({
+    'ID': [1, 2, 3, 4],
     'Name': ['Alice', 'Bob', 'Charlie', 'David']
 })
 
-conn=sqlite3.connect('mydatabase.db')
+# create a connection
+conn = sqlite3.connect('mydatabase.db')
 
+# Use the to_sql method to save the DataFrame to a table in the database
 data.to_sql(
     'client', conn,
-    if_exists = 'replace',
+    if_exists='replace',
     index=False
 )
 
+# closing the connection
 conn.close()
 
+
+########## 
 conn = sqlite3.connect('mydatabase.db')
-cursor = conn.cursor()
-query = """
-    SELECT name 
+
+query = """ 
+    SELECT Name 
     FROM client
     WHERE name IN ('Alice', 'David')
 """
 
-pd.read_sql_query(query,conn)
 
+pd.read_sql_query(query, conn)
--- functions/create_db.py
+++ functions/create_db.py
@@ -1,16 +1,19 @@
-
 import sqlite3
 
-def create_db(
-    database_name: str
+def create_db(  
+    database_name:str      
 ) -> sqlite3.Connection:
     """
-    Create a new SQLite database or connect to an existing one.
+    Creates a SQLite database with the specified name and returns 
+    a connection to it.
 
     Args:
-    database_name (str): The name of the database to create or connect to.
+        database_name (str): The name of the database to create. The function 
+            will append '.db' to this name to create the SQLite database file.
 
     Returns:
-    sqlite3.Connection: A connection object to the SQLite database.
+        sqlite3.Connection: A connection object to the newly created 
+            SQLite database.
     """
+
     return sqlite3.connect(f'{database_name}.db')
